71a63b9743410f30a15a815adbc111b61bc385db
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2024-11-02 23:14:27 -0300

    create project

---
+++ stockmanager/src/App.tsx
@@ -0,0 +1,28 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserRegistration from '../src/components/user/UserRegistration';
+import Login from '../src/components/login/Login';
+import StockManagement from '../src/components/stock/StockManagement';
+import ProtectedRoute from '../src/routes/ProtectedRoutes';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> 
+        <Route path="/register" element={<UserRegistration />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <StockManagement />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
