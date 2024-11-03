ebfd1bf5e4e70b121d65af967c5c469ab952bdb8
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2024-11-02 23:15:57 -0300

    Created forms and routes

---
+++ stockmanager/src/components/login/Login.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'; 
+import './Login.css'; 
+import inventoryImage from '../../assets/inventory.avif';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:4000/api/auth', {
+        email,
+        password,
+      });
+
+      const { token } = response.data;
+
+      console.log("O token captado pelo navegador web é = " + token)
+
+      localStorage.setItem('token', token);
+
+      navigate('/dashboard');
+    } catch (error: any) {
+      setErrorMessage('Login falhou!'); 
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="login-wrapper">
+      <div className="login-image">
+        <img src={inventoryImage} alt="Uma pessoa gerenciando o estoque com um tablet" />
+      </div>
+      <div className='aside'>
+        <div className="login-container">
+          <h2>Login</h2>
+          <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
+            <div>
+              <label htmlFor="email">Email</label> 
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} 
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Senha</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+            {errorMessage && <p className="error-message">{errorMessage}</p>} 
+          </form>
+        </div>
+        <button className="register-button" onClick={() => navigate('/register')}>
+          Cadastre-se.
+        </button>
+      </div>  
+    </div>
+  );
+};
+
+export default Login;
---
+++ stockmanager/src/components/user/UserRegistration.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';  
+import './UserRegistration.css';
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserRegistration: React.FC = () => {
+  const [formData, setFormData] = useState<User>({ name: '', email: '', password: '' });
+  const [message, setMessage] = useState<string | null>(null);
+  const navigate = useNavigate();  
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/api/user', formData);
+      setMessage(`Cadastro criado com sucesso!`);
+      setFormData({ name: '', email: '', password: '' });
+
+      setTimeout(() => {
+        navigate('/'); 
+      }, 4000);
+    } catch (error: any) {
+      if (error.response && error.response.status === 400) {
+        setMessage('Erro ao criar usuário.');
+      } else {
+        setMessage('Erro ao criar usuário');
+      }
+    }
+  };
+
+  return (
+    <div className="container user-registration-container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="user-registration-card">
+            <h2 className="user-registration-title">Cadastrar Usuário</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group user-registration-form-group">
+                <label htmlFor="name">Nome</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="form-control"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group user-registration-form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-control"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group user-registration-form-group">
+                <label htmlFor="password">Senha</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="form-control"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-primary user-registration-button">Cadastrar</button>
+            </form>
+            {message && <p className="user-registration-message">{message}</p>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserRegistration;
